d67d9cc96badbec985a10211a45d94ddcd99af27
Liu Yi <61upup@gmail.com>
2015-08-01 01:23:38 +0800

    Add origin code

--- PicPx/AppDelegate.swift
+++ PicPx/AppDelegate.swift
@@ -15,13 +15,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        let url = "http://picpx.com/uptoken"
+        let params = ["image": "image.png"]
+        
+        post(url, params: params) { (data, response, error) -> Void in
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary
+                print("\(json)")
+            } catch let error as NSError {
+                print("error:\(error)")
+            } catch {
+                print("parse data error")
+            }
+        }
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
+    
+    func post(url: String, params: Dictionary<String, String>, completionHandler: ((NSData?, NSURLResponse?, NSError?) -> Void)?) {
+        var components: [(String, String)] = []
+        
+        for key in Array(params.keys).sort(<) {
+            let value: String! = params[key]
+            components += [(key, value!)]
+        }
 
-
+        let pair = components.map { "\($0)=\($1)" } as [String]
+        let paramString = "&".join(pair)
+        let data = paramString.dataUsingEncoding(NSUTF8StringEncoding)
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        
+        request.HTTPMethod = "POST"
+        request.HTTPBody = data
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: completionHandler!)!
+        
+        task.resume()
+    }
 }
 
