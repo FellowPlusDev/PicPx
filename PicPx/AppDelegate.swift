1dcd06d03d7781c3eebc2329a5f46cb81ade6d0f
Liu Yi <61upup@gmail.com>
2015-07-31 20:34:24 +0800

    Initial Commit

---
+++ PicPx/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PicPx
+//
+//  Created by im61 on 7/31/15.
+//  Copyright © 2015 Fellow Plus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
